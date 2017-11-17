d70f9392a31be2650c09f4ea95d4f28dfeb081c5
juga0 <juga@riseup.net>
2017-11-17 21:40:51 +0000

    Add setup and version

---
+++ setup.py
@@ -0,0 +1,44 @@
+import os
+from setuptools import setup
+
+
+def main():
+    with open(os.path.join("autocrypt", "__init__.py")) as f:
+        for line in f:
+            if "__version__" in line.strip():
+                version = line.split("=", 1)[1].strip().strip('"')
+                break
+
+    with open("README.rst") as f:
+        long_desc = f.read()
+
+    setup(
+        name='pyac',
+        description='Autocrypt Level 1 implemention using PGPy',
+        long_description=long_desc,
+        version=version,
+        url='https://github.com/juga0/pyac',
+        license='MIT license',
+        platforms=['unix', 'linux', 'osx', 'cygwin', 'win32'],
+        author='juga',
+        author_email='juga@riseup.net',
+        classifiers=['Development Status :: 3 - Alpha',
+                     'Intended Audience :: Developers',
+                     'License :: OSI Approved :: MIT License',
+                     'Operating System :: POSIX',
+                     'Operating System :: MacOS :: MacOS X',
+                     'Topic :: Utilities',
+                     'Intended Audience :: Developers',
+                     'Programming Language :: Python'],
+        packages=['autocrypt'],
+        entry_points='''
+            [console_scripts]
+            examplel1=autocrypt.examplel1:main
+        ''',
+        install_requires=["click>=6.0", "six", "PGPy>=0.4.1", "emailpgp"],
+        zip_safe=False,
+    )
+
+
+if __name__ == '__main__':
+    main()
